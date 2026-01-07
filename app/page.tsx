50fb10514a049e56d46fcec4053feb3b2289aaec
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2026-01-07 10:59:21 +0600

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Commingsoon from "@/components/web/Commingsoon";
-import Footer from "@/components/web/Footer";
-import Navbar from "@/components/web/Navbar";
+// import Footer from "@/components/web/Footer";
+// import Navbar from "@/components/web/Navbar";
 
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
-      <Navbar />
+      {/* <Navbar /> */}
       <Commingsoon/>
-      <Footer />
+      {/* <Footer /> */}
     </main>
   )
 }
