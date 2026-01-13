89f5ab0ce766eb29bd1702dc42661d3f90e97c1c
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2026-01-13 14:34:25 +0600

    add

--- components/school/Minutes.tsx
+++ components/school/Minutes.tsx
@@ -18,7 +18,7 @@ const Minutes = () => {
             Set up in minutes
           </Button>
           <div className='mt-2'>
-            <h3 className="text-2xl sm:text-[52px] leading-9 lg:leading-[60px]  font-bold mb-4">The Starting Point Of Your  Career <span className='text-[#E4E403]'>Transforming Ambition</span> Into Real Opportunity</h3>
+            <h3 className="text-2xl sm:text-[52px] leading-9 lg:leading-[60px]  font-bold mb-4">The Starting Point Of Your  Career, <span className='text-[#E4E403]'>Transforming Ambition</span> Into Real Opportunity</h3>
             <p className='max-w-2xl text-[#5A5A5A]'>Aspiring Legal Network is a careers and education platform for <br className='sm:block hidden' /> students of all ages. Through practical legal experiences, employer <br className='sm:block hidden'/> led sessions, and AI powered tools, students develop real insight,<br className='sm:block hidden'/> skills, and confidence to progress across work experience,<br className='sm:block hidden'/> apprenticeships, and university pathways, with progress tracked in <br className='sm:block hidden'/> one place.</p>
           </div>
           <div className='lg:mt-10 mt-4 '>
